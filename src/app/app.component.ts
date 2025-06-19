36c6cd1e6903a64921b77f91f7226338faa0db41
Amexa777 <gasanbekov.amir@mail.ru>
2025-06-19 13:57:47 +0300

    добавил изменения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const aboutCompanyFn = (text: string) => text;
+
+const aboutCompany = aboutCompanyFn ('О компании');
+
+const newPages = [5, 4, 3, 2, 1];
+
+const menuItems  = ['Каталог','Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда'];
+
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mentoring-first-project';
+  title: string = 'mentoring-first-project';
+  
+  isShowAboutCompany = true;
+  
+  isShowCatalog = true;
+  
+  readonly headerItem3 = 'Каталог';
+  
+  isShowImg = true;
+  
+  readonly newPages = newPages;
+  
+  readonly aboutCompany = aboutCompany;
+  
+  isUpperCase = true;
+  
+
+
 }
+
